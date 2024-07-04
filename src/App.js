f7f19df81123fad41d768994662bd3e14191ff19
Abhijith <abhijithtp2003@gmail.com>
2024-07-04 16:30:25 +0530

    login commented

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import User from './User/App'
-import Guest from './Guest/App'
+// import Guest from './Guest/App'
 import './App.css'
 
 const App = () => {
   return (
     <div>
       <Routes>
-        <Route path="/*" element={<Guest/>} />
+        {/* <Route path="/*" element={<Guest/>} /> */}
         <Route path="/User/*" element={<User/>} />
       </Routes>
     </div>
